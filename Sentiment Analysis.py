de1ee3ac264097ae51ecb3acee4f2cc9854a4e23
JarTheHead <jared78.ho@gmail.com>
2023-02-02 14:36:16 -0800

    Update Sentiment Analysis.py
    Added a loop to continue testing until prompted to stop.

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -9,6 +9,11 @@ def sentiment_analysis(text):
     else:
         return "negative"
 
-text = input("Enter a sentence: ")
-sentiment = sentiment_analysis(text)
-print("The sentiment is:", sentiment)
+while True:
+    text = input("Enter a sentence (Enter 'q' to quit): ")
+    if text == 'q':
+        break
+    sentiment = sentiment_analysis(text)
+    print("The sentiment is:", sentiment)
+
+print("Bye!")
